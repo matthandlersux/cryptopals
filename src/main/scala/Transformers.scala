f3f25fd631b42818ef9c2e19edbfbc899157f96a
matt handler <matt.handler@gmail.com>
2014-11-19 18:16:31 -0500

    start working on problem 13

--- src/main/scala/Transformers.scala
+++ src/main/scala/Transformers.scala
@@ -2,6 +2,8 @@ package helpers
 
 import sun.misc.{BASE64Encoder, BASE64Decoder}
 import scala.util.Random
+import play.api.libs.json.{JsObject, Json}
+import java.net.{URLEncoder, URLDecoder}
 
 object Transformers {
 
@@ -25,5 +27,19 @@ object Transformers {
 
   def parseHex(string: String): Int =
     Integer.parseInt(string, 16)
+
+  def parseQueryString(string: String): Option[JsObject] = {
+    val Pattern = """([^=]+)=(.*)""".r
+    val pairs = string split '&' collect {
+      case Pattern(key, value) => (key, URLDecoder.decode(value))
+    }
+
+    Json.toJson(pairs.toMap).asOpt[JsObject]
+  }
+
+  def jsonToQueryString(json: JsObject): String =
+    json.fields map { case (key, jsValue) =>
+      key + '=' + URLEncoder.encode(jsValue.toString)
+    } mkString "&"
 
 }
